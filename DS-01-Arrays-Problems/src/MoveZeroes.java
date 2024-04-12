082c0bac49f0b654e1d113b8f03ef839521bcf92
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-04-11 20:47:56 -0500

    Data Structures and Algorithms: Arrays Problems

---
+++ DS-01-Arrays-Problems/src/MoveZeroes.java
@@ -0,0 +1,22 @@
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] == 0) continue;
+            nums[j] = nums[i];
+            j++;
+        }
+
+        while (j < nums.length){
+            nums[j] = 0;
+            j++;
+        }
+
+
+
+    }
+
+
+    }
+

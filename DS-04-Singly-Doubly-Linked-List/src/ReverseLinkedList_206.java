974bd90aa289d764083441b8620f940c252bf78a
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-05-15 18:27:21 -0500

    ReverseLinkedList

---
+++ DS-04-Singly-Doubly-Linked-List/src/ReverseLinkedList_206.java
@@ -0,0 +1,29 @@
+public class ReverseLinkedList_206 {
+    class ListNode{
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+    }
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while(current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+
+    }
+
+}

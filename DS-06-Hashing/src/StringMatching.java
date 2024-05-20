15cdce9a2bf98fd98873aa6e5470f3cffbd532da
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-05-20 12:34:56 -0500

    Your commit message

---
+++ DS-06-Hashing/src/StringMatching.java
@@ -0,0 +1,11 @@
+public class StringMatching {
+    // Given a String a and a pattern, count the number of positions where there the pattern occurs in the string
+    // Input: sjhfppffpp
+    // pp
+    // Output:2
+
+    /*
+    Brute-force solution: iterate over each m char in the string, count it's occurrence of pattern in a string
+    Rabin-Karp hashing algorithm
+     */
+}

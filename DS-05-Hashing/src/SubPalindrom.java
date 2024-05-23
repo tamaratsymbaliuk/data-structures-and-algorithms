74d8952b9bfcf9f9ebc9ff9437dba4fdd4708db8
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-05-22 22:14:43 -0500

    Hashing, SubPalindrom

---
+++ DS-05-Hashing/src/SubPalindrom.java
@@ -0,0 +1,15 @@
+public class SubPalindrom {
+    /*
+    Find out how many subsctrings of a given string are palindromes
+    ABACABA - 
+    A
+    B
+    C
+    ABA
+    ABACABA
+    ACA
+    BACAB
+     */
+
+
+}

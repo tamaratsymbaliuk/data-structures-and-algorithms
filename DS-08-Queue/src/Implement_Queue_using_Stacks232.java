e2720db0cd673493e271cd3d89171c58a93cf793
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-06-17 16:15:31 -0500

    DS-08-Queue - Implement_Queue_using_Stacks232

---
+++ DS-08-Queue/src/Implement_Queue_using_Stacks232.java
@@ -0,0 +1,46 @@
+import java.util.Stack;
+
+public class Implement_Queue_using_Stacks232 {
+    /*
+    https://leetcode.com/problems/implement-queue-using-stacks/description/
+     */
+
+    Stack<Integer> input = new Stack();
+    Stack<Integer> output = new Stack();
+
+    public Implement_Queue_using_Stacks232() {
+
+    }
+
+    public void push(int x) {
+        input.push(x);
+    }
+
+    public int pop() {
+        int x = peek();
+        output.pop();
+        return x;
+    }
+
+    public int peek() {
+        if (output.empty())
+            while (!input.empty())
+                output.push(input.pop());
+        return output.peek();
+
+    }
+
+    public boolean empty() {
+        return input.empty() && output.empty();
+
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */

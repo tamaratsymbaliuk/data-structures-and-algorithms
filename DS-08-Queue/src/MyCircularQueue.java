d449905c6e41c886678d3aefadb529927e459007
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-06-20 22:14:26 -0500

    Design Circular Queue

---
+++ DS-08-Queue/src/MyCircularQueue.java
@@ -0,0 +1,46 @@
+public class MyCircularQueue {
+    // https://leetcode.com/problems/design-circular-queue/
+    int maxSize, size = 0;
+    ListNode head = null, tail = null;
+    public MyCircularQueue(int k) {
+        maxSize = k;
+    }
+    public boolean enQueue(int val) {
+        if (isFull()) return false;
+        ListNode newNode = new ListNode(val, null);
+        if (isEmpty()) head = tail = newNode;
+        else {
+            tail.next = newNode;
+            tail = tail.next;
+        }
+        size++;
+        return true;
+    }
+    public boolean deQueue() {
+        if (isEmpty()) return false;
+        head = head.next;
+        size--;
+        return true;
+    }
+    public int Front() {
+        return isEmpty() ? -1 : head.val;
+    }
+    public int Rear() {
+        return isEmpty() ? -1 : tail.val;
+    }
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    public boolean isFull() {
+        return size == maxSize;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}

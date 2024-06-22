7cce9cfe86f349f6b42346bc8c89b89586a1aaee
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-06-21 21:25:31 -0500

    MyCircularQueueu

---
+++ DS-08-Queue/src/FrontMiddleBackQueue.java
@@ -0,0 +1,50 @@
+import java.util.LinkedList;
+
+public class FrontMiddleBackQueue {
+    // https://leetcode.com/problems/design-front-middle-back-queue/
+
+    LinkedList<Integer> list;
+
+    public FrontMiddleBackQueue() {
+        list = new LinkedList();
+    }
+
+    public void pushFront(int val) {
+        list.add(0, val);
+    }
+
+    public void pushMiddle(int val) {
+        list.add(list.size() / 2, val);
+    }
+
+    public void pushBack(int val) {
+        list.add(val);
+    }
+
+    public int popFront() {
+        if(isEmpty()) return -1;
+
+        return list.remove(0);
+    }
+
+    public int popMiddle() {
+        if(isEmpty()) return -1;
+
+        return list.remove(findMiddleIndex());
+    }
+
+    public int popBack() {
+        if(isEmpty()) return -1;
+
+        return list.remove(list.size() - 1);
+    }
+
+    private boolean isEmpty(){
+        return list.isEmpty();
+    }
+
+    private int findMiddleIndex(){
+        return (list.size() - 1) / 2;
+    }
+
+}

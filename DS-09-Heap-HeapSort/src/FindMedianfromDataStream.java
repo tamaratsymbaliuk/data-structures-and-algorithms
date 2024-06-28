c1ab2059b64ac7fbc2114f3ac1068b6bcd90d228
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-06-27 22:30:45 -0500

    FindMedianfromDataStream

---
+++ DS-09-Heap-HeapSort/src/FindMedianfromDataStream.java
@@ -0,0 +1,9 @@
+import java.util.PriorityQueue;
+
+public class FindMedianfromDataStream {
+    // https://leetcode.com/problems/find-median-from-data-stream/description/
+    // solution https://leetcode.com/problems/find-median-from-data-stream/solutions/1330646/c-java-python-minheap-maxheap-solution-picture-explain-clean-concise/
+
+    //second half of the sorted array
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+}

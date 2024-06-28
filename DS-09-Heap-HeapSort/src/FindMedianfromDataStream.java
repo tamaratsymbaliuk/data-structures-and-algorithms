da709e5a9b344131fba3b75cbb4e5d18dab791d1
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-06-27 22:36:37 -0500

    FindMedianfromDataStream

--- DS-09-Heap-HeapSort/src/FindMedianfromDataStream.java
+++ DS-09-Heap-HeapSort/src/FindMedianfromDataStream.java
@@ -1,3 +1,5 @@
+import java.util.Collection;
+import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class FindMedianfromDataStream {
@@ -6,4 +8,24 @@ public class FindMedianfromDataStream {
 
     //second half of the sorted array
     PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    // first half of the sorted array
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+    public FindMedianfromDataStream() {}
+
+    public void addNum(int num) {
+        maxHeap.offer(num);
+        minHeap.offer(maxHeap.poll());
+        if (minHeap.size() > maxHeap.size()) {
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (maxHeap.size() > minHeap.size()) { // odd number of elements in the array
+            return maxHeap.peek();
+        } else { // even number of elements in the array because maxHeap.size() == minHeap.size()
+            return (minHeap.peek() + maxHeap.peek()) / 2.0d;
+        }
+
+    }
 }

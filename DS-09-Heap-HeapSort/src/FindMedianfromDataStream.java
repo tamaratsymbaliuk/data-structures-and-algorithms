b0ee5dd08f6d650c8542ca1c0dbb632a54a5187d
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-06-27 22:39:17 -0500

    FindMedianfromDataStream

--- DS-09-Heap-HeapSort/src/FindMedianfromDataStream.java
+++ DS-09-Heap-HeapSort/src/FindMedianfromDataStream.java
@@ -5,6 +5,7 @@ import java.util.PriorityQueue;
 public class FindMedianfromDataStream {
     // https://leetcode.com/problems/find-median-from-data-stream/description/
     // solution https://leetcode.com/problems/find-median-from-data-stream/solutions/1330646/c-java-python-minheap-maxheap-solution-picture-explain-clean-concise/
+    // https://leetcode.com/problems/find-median-from-data-stream/ I find it better
 
     //second half of the sorted array
     PriorityQueue<Integer> minHeap = new PriorityQueue<>();

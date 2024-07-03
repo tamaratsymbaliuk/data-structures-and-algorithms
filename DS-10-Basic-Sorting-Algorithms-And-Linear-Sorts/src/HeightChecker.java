c035e44564c9253b8a110639d0898712446c229b
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-07-03 16:53:56 -0500

    HeightChecker

--- DS-10-Basic-Sorting-Algorithms-And-Linear-Sorts/src/HeightChecker.java
+++ DS-10-Basic-Sorting-Algorithms-And-Linear-Sorts/src/HeightChecker.java
@@ -1,3 +1,15 @@
 public class HeightChecker {
     // https://leetcode.com/problems/height-checker/description/
+
+    /*
+    Bubble Sort -
+    Iterate over the array (possibly multiple times, up to [n - 1] times)
+    if a[i] > a[i + 1]
+    swap(a[i], a[i + 1]
+    swapped = true
+    After iterating over the array, check swapped
+    If swapped is true, iterate over the array again
+    else
+    stop the algorithm, array is sorted
+     */
 }

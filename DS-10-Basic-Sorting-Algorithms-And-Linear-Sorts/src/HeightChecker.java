27009c30d4f9113fabd1eecdcf037c3b774bf1fc
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-07-03 17:16:46 -0500

    HeightChecker

--- DS-10-Basic-Sorting-Algorithms-And-Linear-Sorts/src/HeightChecker.java
+++ DS-10-Basic-Sorting-Algorithms-And-Linear-Sorts/src/HeightChecker.java
@@ -12,4 +12,46 @@ public class HeightChecker {
     else
     stop the algorithm, array is sorted
      */
+    public static void main(String[] args) {
+        HeightChecker test = new HeightChecker();
+        int[] array = {1,1,4,2,1,3};
+        System.out.println(test.heightChecker(array));
+    }
+
+    public int heightChecker(int[] heights) {
+        int n = heights.length;
+
+        int[] sorted_copy = new int[n];
+        for (int i = 0; i < n; i++) {
+            sorted_copy[i] = heights[i];
+        }
+        while (true) {
+            boolean swapped = false;
+            for (int i = 0; i < n - 1; i++) {
+                if (sorted_copy[i] > sorted_copy[i + 1]) {
+                    swap(sorted_copy, i, i + 1);
+                    swapped = true;
+                }
+            }
+            if (!swapped) { // the array is sorted
+                break;
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            if (sorted_copy[i] != heights[i]) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+
 }

195687d757021d5a6f75c1231c448f6ac0cf56d6
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-10-01 22:34:54 -0500

    TwoDArray

---
+++ DS-01-Arrays-Problems/src/TwoDArray.java
@@ -0,0 +1,21 @@
+public class TwoDArray {
+    public static void main(String[] args) {
+        int[][] array = new int[6][7];
+        int start = 1;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = start++;
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println(array[5][6]);
+    }
+}

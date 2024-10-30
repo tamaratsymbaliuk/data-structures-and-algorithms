a487b40fd36b9db96c217c240a35aaa36b8cd293
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-10-29 21:52:03 -0500

    Implement Queue using Stack problem

--- DS-08-Queue/src/Implement_Queue_using_Stacks232.java
+++ DS-08-Queue/src/Implement_Queue_using_Stacks232.java
@@ -2,6 +2,17 @@ import java.util.Stack;
 
 public class Implement_Queue_using_Stacks232 {
     public static void main(String[] args) {
+        Implement_Queue_using_Stacks232 obj = new Implement_Queue_using_Stacks232();
+        obj.push(5);
+        obj.push(8);
+        obj.push(9);
+
+        int param_2 = obj.pop();
+        int param_3 = obj.peek();
+        boolean param_4 = obj.empty();
+        System.out.println(param_2);
+        System.out.println(param_3);
+        System.out.println(param_4);
 
     }
     /*
@@ -10,10 +21,6 @@ public class Implement_Queue_using_Stacks232 {
 
     Stack<Integer> input = new Stack();
     Stack<Integer> output = new Stack();
-
-    public Implement_Queue_using_Stacks232() {
-
-    }
 
     public void push(int x) {
         input.push(x);

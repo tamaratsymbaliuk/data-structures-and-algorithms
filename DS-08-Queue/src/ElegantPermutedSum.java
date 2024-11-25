5be0b998bf9c8803110d174df1b5a87fbb2516bb
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-11-24 19:47:20 -0600

    ElegantPermutedSum

---
+++ DS-08-Queue/src/ElegantPermutedSum.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class ElegantPermutedSum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+        System.out.printf("%d %d\n", number/10, number%10);
+    }
+
+    public int solution(int[] nums) {
+        Arrays.sort(nums);
+
+        Deque<Integer> maxDeque = new LinkedList<>();
+        Deque<Integer> minDeque = new LinkedList<>();
+
+        int n = nums.length;
+        for (int i = 0; i < n / 2; ++i) {
+            minDeque.offerLast(nums[i]);
+            maxDeque.offerLast(nums[n - i - 1]);
+        }
+
+    }
+}
+

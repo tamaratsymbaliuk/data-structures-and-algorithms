7d58ac819485f918953e9f0ac7cbc7ac354945b3
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-12-28 21:36:30 -0600

    MedianFinder

---
+++ DS-09-Heap-HeapSort/src/MedianFinder.java
@@ -0,0 +1,21 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class MedianFinder {
+    PriorityQueue<Integer> ordered = new PriorityQueue<>();
+    PriorityQueue<Integer> sorted = new PriorityQueue<>(Collections.reverseOrder());
+
+    public MedianFinder() {}
+
+    void addNum(int num) {
+        ordered.offer(num);
+        sorted.offer(num);
+
+    }
+
+    double findMedian() {
+           return ordered.peek() + sorted.peek() / 2.0d;
+        }
+    }
+
+}

cb6ca7574e95d7afa3bc3f7b98330d48dd3c6b79
tamaratsymbaliuk <tsymbalt14@gmail.com>
2025-01-21 22:27:06 -0600

    StringCompression

--- DS-14-Week18/src/twoPointers/StringCompression.java
+++ DS-14-Week18/src/twoPointers/StringCompression.java
@@ -35,7 +35,6 @@ public class StringCompression {
             streakStart = streakEnd + 1;
         }
         return indexAnswer;
-
     }
     public static void main(String[] args) {
         char[] chars = {'a','a','b','b','c','c','c'};
